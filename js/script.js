4c202d817c2ff667ebd01a837aa4ec1a5eebf32f
orishameedah <orishameedah@gmail.com>
2022-08-08 21:28:28 -0700

    vertex diagnosis

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 const showMenu = (toggleId, navId)=>{
     const toggle = document.getElementById(toggleId),
         nav = document.getElementById(navId)
-        console.log(toggle);
-        console.log(nav);
+        // console.log(toggle);
+        // console.log(nav);
         
         if(toggle && nav){
             toggle.addEventListener('click', ()=>{
@@ -14,7 +14,7 @@ showMenu('menu-btn','nav_menu')
 
 const navLink = document.querySelectorAll('.nav__link');
 
-console.log({ navLink });
+// console.log({ navLink });
 
 function linkAction() {
     const navMenu = document.getElementById('nav_menu');
